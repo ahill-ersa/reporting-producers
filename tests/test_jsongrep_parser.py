e3995e830e0b6445bcccaa6f3906f5048187557d
Shunde Zhang <shunde.p.zhang@gmail.com>
2015-05-20 13:38:40 +0930

    changed tests to work under python 2.6

--- tests/test_jsongrep_parser.py
+++ tests/test_jsongrep_parser.py
@@ -14,7 +14,12 @@ class JsonGrepParserTestCase(unittest.TestCase):
         self.assertTrue('timestamp' in output)
         self.assertTrue('hostname' in output)
         self.assertTrue('hcp-chargeback' in output)
-        self.assertDictEqual({"hcp-chargeback": output["hcp-chargeback"]}, {"hcp-chargeback": [{u'systemName':u'hcp1.s3.ersa.edu.au'}, {u'systemName':u'hcp2.s3.ersa.edu.au'}]})
+        self.assertTrue(isinstance(output["hcp-chargeback"], list))
+        self.assertTrue(len(output["hcp-chargeback"])==2)
+        self.assertTrue('systemName' in output["hcp-chargeback"][0])
+        self.assertTrue(output["hcp-chargeback"][0]['systemName']=='hcp1.s3.ersa.edu.au')
+        self.assertTrue('systemName' in output["hcp-chargeback"][1])
+        self.assertTrue(output["hcp-chargeback"][1]['systemName']=='hcp2.s3.ersa.edu.au')
 
     def test_case2(self):
         parser=JsonGrepParser(pattern="chargebackData", list_name="hcp-chargeback")
@@ -24,7 +29,10 @@ class JsonGrepParserTestCase(unittest.TestCase):
         self.assertTrue('timestamp' in output)
         self.assertTrue('hostname' in output)
         self.assertTrue('hcp-chargeback' in output)
-        self.assertDictEqual({"hcp-chargeback": output["hcp-chargeback"]}, {"hcp-chargeback": [u'chargebackData1',u'chargebackData2']})
+        self.assertTrue(isinstance(output["hcp-chargeback"], list))
+        self.assertTrue(len(output["hcp-chargeback"])==2)
+        self.assertTrue(output["hcp-chargeback"][0]=='chargebackData1')
+        self.assertTrue(output["hcp-chargeback"][1]=='chargebackData2')
 
     def test_case3(self):
         parser=JsonGrepParser(pattern=". chargebackData", list_name="hcp-chargeback")
@@ -34,18 +42,10 @@ class JsonGrepParserTestCase(unittest.TestCase):
         self.assertTrue('timestamp' in output)
         self.assertTrue('hostname' in output)
         self.assertTrue('hcp-chargeback' in output)
-        self.assertDictEqual({"hcp-chargeback": output["hcp-chargeback"]}, {"hcp-chargeback": [1, 2]})
+        self.assertTrue(isinstance(output["hcp-chargeback"], list))
+        self.assertTrue(len(output["hcp-chargeback"])==2)
+        self.assertTrue(output["hcp-chargeback"][0]==1)
+        self.assertTrue(output["hcp-chargeback"][1]==2)
 
-    def assertDictEqual(self, d1, d2, msg=None): # assertEqual uses for dicts
-        for k,v1 in d1.iteritems():
-            self.assertIn(k, d2, msg)
-            v2 = d2[k]
-            if(isinstance(v1, collections.Iterable) and
-               not isinstance(v1, basestring)):
-                self.assertItemsEqual(v1, v2, msg)
-            else:
-                self.assertEqual(v1, v2, msg)
-        return True
-            
 if __name__ == '__main__':
     unittest.main()
